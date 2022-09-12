8302662fb8f00603fbf03425ba3b7aafc101ed40
zhizhizhizhizhi <zhizhi.xu@yahoo.com>
2022-09-13 03:18:20 +0800

    change api

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ const db = require('./db')
 app.get('/', (request, response) => {
     response.json({ info: 'Test' })
 })
-app.get('/teams', db.getTeams)
-app.post('/teams', db.createTeam)
-app.post('/teams/batch', db.createTeams)
-app.put('/teams', db.updateTeam)
-app.get('/matches', db.getMatches)
-app.post('/matches', db.createMatchWithUpdate)
-app.post('/matches/batch', db.createMatchesWithUpdate)
-app.delete('/', db.deleteAll)
+app.get('/api/teams', db.getTeams)
+app.post('/api/teams', db.createTeam)
+app.post('/api/teams/batch', db.createTeams)
+app.put('/api/teams', db.updateTeam)
+app.get('/api/matches', db.getMatches)
+app.post('/api/matches', db.createMatchWithUpdate)
+app.post('/api/matches/batch', db.createMatchesWithUpdate)
+app.delete('/api/deleteAll', db.deleteAll)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
+
+const path = require('path');
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
